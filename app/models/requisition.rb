2493241d449c075ca992e6fa91ee9acf5425d4c0
Mike Danko <danko@theorypump.com>
2012-06-14 10:22:21 -0400

    added .rvmrc

--- app/models/requisition.rb
+++ app/models/requisition.rb
@@ -3,5 +3,5 @@ class Requisition < ActiveRecord::Base
   validates :non_ip_interfaces, :inclusion => {:in => ['true', 'false']}, :presence => true
   validates :non_ip_snmp_primary, :inclusion => {:in => ['Y', 'N']}, :presence => true
   has_many :nodes
-  attr_accessible :name, :non_ip_interfaces, :non_ip_snmp_primary
+  attr_accessible :name, :non_ip_interfaces, :non_ip_snmp_primary, :node_ids
 end
