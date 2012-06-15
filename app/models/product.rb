e62a0c4151f093d2ce350fc9bc57359b34fa9069
Mike Danko <danko@theorypump.com>
2012-06-15 13:22:25 -0400

    added node counts to products and manu

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => true
   belongs_to :manufacturer
+  has_many :nodes
   attr_accessible :model_number, :name
 end
