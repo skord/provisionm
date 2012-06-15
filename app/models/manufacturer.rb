f042a08a61ff705855170ed5d017b69d1821511d
Mike Danko <danko@theorypump.com>
2012-06-15 13:17:16 -0400

    other side of relationship

--- app/models/manufacturer.rb
+++ app/models/manufacturer.rb
@@ -1,6 +1,7 @@
 class Manufacturer < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => true
   has_many :products, :dependent => :destroy
+  has_many :nodes
   accepts_nested_attributes_for :products, :reject_if => :all_blank, :allow_destroy => true
   attr_accessible :name, :products_attributes
 end
--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,6 +1,7 @@
 class Vendor < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => true
   has_many :support_contracts
+  has_many :nodes
   accepts_nested_attributes_for :support_contracts, :reject_if => :all_blank, :allow_destroy => true
   attr_accessible :fax, :name, :phone, :support_contracts_attributes
 end
