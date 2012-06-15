107a04b985dfec39302d52cb5b8cd1be06ed9cbf
Mike Danko <danko@theorypump.com>
2012-06-15 08:44:09 -0400

    doh, delete the orphan products

--- app/models/manufacturer.rb
+++ app/models/manufacturer.rb
@@ -1,6 +1,6 @@
 class Manufacturer < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => true
-  has_many :products
+  has_many :products, :dependent => :destroy
   accepts_nested_attributes_for :products, :reject_if => :all_blank, :allow_destroy => true
   attr_accessible :name, :products_attributes
 end
